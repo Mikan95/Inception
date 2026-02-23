3a7ca43420f88ed073640c3d7aa8dd480c1a1b8b
Andrew Meechan <andrewmeechan@hotmail.com>
2026-02-23 16:20:44 +0100

    removed admin username check from wp entrypoint.sh

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -8,21 +8,6 @@ DB_PASSWORD=$(cat ${MYSQL_PASSWORD_FILE})
 WP_ADMIN_USER=$(cat ${WP_ADMIN_PASSWORD_FILE} | cut -d: -f1)
 WP_ADMIN_PASS=$(cat ${WP_ADMIN_PASSWORD_FILE} | cut -d: -f2)
 
-# if [ "$WP_ADMIN_USER" != "$WP_ADMIN_USER_ENV" ]; then
-#     echo "Error: WP_ADMIN_USER_ENV must match the saved credentials in secrets!"
-#     exit 1
-# fi
-# lower_user=$(printf "%s" "${WP_ADMIN_USER}" | tr '[:upper]' '[:lower]');
-# case "$lower_user" in
-# 	*admin*)
-# 		echo "Error: Username cannot contain 'admin' (case-insensitive)!\n";
-# 		exit 1 ;;
-# esac;
-# else
-#     echo "WP_ADMIN_USER matches and is valid!"
-# fi
-
-
 # ── Wait for MariaDB to be ready ─────────────────────────────────────────────────────────────
 echo ">>> Waiting for MariaDB..."
 # nc = netcat // -z = just try to connect, don't write or read data
